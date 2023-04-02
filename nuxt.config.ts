c7510d1723ef33d381034eec8a5ae8e41223d59a
taiki-dev-0922 <taiki.dev0922@gmail.com>
2023-04-02 17:51:38 +0900

    tailwindcss削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,2 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  buildModules: ["@nuxtjs/tailwindcss"],
-  css: ["@/assets/css/tailwind.css"], // <= ドキュメントからさらに追加
-});
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({});
--- pages/master/villages/list.vue
+++ pages/master/villages/list.vue
@@ -1,233 +1,163 @@
 <template>
-  <div class="container-fluid">
-    <wj-accordion
-      :showIcons="true"
-      :autoSwitch="true"
-      :isAnimated="true"
-      :allowCollapseAll="true"
-      :allowExpandMany="true"
+  <div id="accordionExample">
+    <div
+      class="rounded-t-lg border border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800"
     >
-      <wj-accordion-pane>
-        <!-- pane -->
-        <div class="main-pane">
-          <!-- header -->
-          アコーディオン
-          <div class="desc">アコーディオンのプロパティを変更します。</div>
+      <h2 class="mb-0" id="headingOne">
+        <button
+          class="group relative flex w-full items-center rounded-t-[15px] border-0 bg-white py-4 px-5 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
+          type="button"
+          data-te-collapse-init
+          data-te-target="#collapseOne"
+          aria-expanded="true"
+          aria-controls="collapseOne"
+        >
+          Accordion Item #1
+          <span
+            class="ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </span>
+        </button>
+      </h2>
+      <div
+        id="collapseOne"
+        class="!visible"
+        data-te-collapse-item
+        data-te-collapse-show
+        aria-labelledby="headingOne"
+        data-te-parent="#accordionExample"
+      >
+        <div class="py-4 px-5">
+          <strong>This is the first item's accordion body.</strong> It is shown
+          by default, until the collapse plugin adds the appropriate classes
+          that we use to style each element. These classes control the overall
+          appearance, as well as the showing and hiding via CSS transitions. You
+          can modify any of this with custom CSS or overriding our default
+          variables. It's also worth noting that just about any HTML can go
+          within the <code>.accordion-body</code>, though the transition does
+          limit overflow.
         </div>
-        <div>
-          <!-- content -->
-          <div class="wj-labeled-input switch">
-            <input id="showIcons" type="checkbox" v-model="showIcons" />
-            <label for="showIcons">
-              アイコンを表示 <span>ヘッダーにアイコンを表示します。</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="autoSwitch" type="checkbox" v-model="autoSwitch" />
-            <label for="autoSwitch">
-              自動的に展開
-              <span>矢印キーでパネルを選択したときに自動的に展開します。</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="isAnimated" type="checkbox" v-model="isAnimated" />
-            <label for="isAnimated">
-              アニメーション
-              <span>パネルの展開／折りたたみをアニメーション表示します。</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input
-              id="allowCollapseAll"
-              type="checkbox"
-              v-model="allowCollapseAll"
-            />
-            <label for="allowCollapseAll">
-              すべて折りたたみ可能
-              <span>すべてのパネルを折りたためるようにします。</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input
-              id="allowExpandMany"
-              type="checkbox"
-              v-model="allowExpandMany"
-            />
-            <label for="allowExpandMany">
-              複数のパネルを展開可能
-              <span>一度に複数のパネルを展開できるようにします。</span>
-            </label>
-          </div>
+      </div>
+    </div>
+    <div
+      class="border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800"
+    >
+      <h2 class="mb-0" id="headingTwo">
+        <button
+          class="group relative flex w-full items-center rounded-none border-0 bg-white py-4 px-5 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
+          type="button"
+          data-te-collapse-init
+          data-te-collapse-collapsed
+          data-te-target="#collapseTwo"
+          aria-expanded="false"
+          aria-controls="collapseTwo"
+        >
+          Accordion Item #2
+          <span
+            class="ml-auto -mr-1 h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </span>
+        </button>
+      </h2>
+      <div
+        id="collapseTwo"
+        class="!visible hidden"
+        data-te-collapse-item
+        aria-labelledby="headingTwo"
+        data-te-parent="#accordionExample"
+      >
+        <div class="py-4 px-5">
+          <strong>This is the second item's accordion body.</strong> It is
+          hidden by default, until the collapse plugin adds the appropriate
+          classes that we use to style each element. These classes control the
+          overall appearance, as well as the showing and hiding via CSS
+          transitions. You can modify any of this with custom CSS or overriding
+          our default variables. It's also worth noting that just about any HTML
+          can go within the <code>.accordion-body</code>, though the transition
+          does limit overflow.
         </div>
-      </wj-accordion-pane>
-
-      <wj-accordion-pane>
-        <!-- pane -->
-        <div>
-          <!-- header -->
-          ネットワーク
-          <div class="desc">Wi-Fi、機内モード、インターネット共有</div>
+      </div>
+    </div>
+    <div
+      class="rounded-b-lg border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800"
+    >
+      <h2 class="accordion-header mb-0" id="headingThree">
+        <button
+          class="group relative flex w-full items-center border-0 bg-white py-4 px-5 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)] [&[data-te-collapse-collapsed]]:rounded-b-[15px] [&[data-te-collapse-collapsed]]:transition-none"
+          type="button"
+          data-te-collapse-init
+          data-te-collapse-collapsed
+          data-te-target="#collapseThree"
+          aria-expanded="false"
+          aria-controls="collapseThree"
+        >
+          Accordion Item #3
+          <span
+            class="ml-auto -mr-1 h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </span>
+        </button>
+      </h2>
+      <div
+        id="collapseThree"
+        class="!visible hidden"
+        data-te-collapse-item
+        aria-labelledby="headingThree"
+        data-te-parent="#accordionExample"
+      >
+        <div class="py-4 px-5">
+          <strong>This is the third item's accordion body.</strong> It is hidden
+          by default, until the collapse plugin adds the appropriate classes
+          that we use to style each element. These classes control the overall
+          appearance, as well as the showing and hiding via CSS transitions. You
+          can modify any of this with custom CSS or overriding our default
+          variables. It's also worth noting that just about any HTML can go
+          within the <code>.accordion-body</code>, though the transition does
+          limit overflow.
         </div>
-        <div>
-          <!-- content -->
-          <div class="wj-labeled-input switch">
-            <input id="wifi" type="checkbox" checked />
-            <label for="wifi">
-              Wi-Fi <span>Wi-Fiをオンまたはオフにします。</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="apmode" type="checkbox" />
-            <label for="apmode">
-              機内モード <span>機内モードをオンまたはオフにします。</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="hotspot" type="checkbox" />
-            <label for="hotspot">
-              機内モード <span>機内モードをオンまたはオフにします。</span>
-            </label>
-          </div>
-        </div>
-      </wj-accordion-pane>
-
-      <wj-accordion-pane>
-        <!-- pane -->
-        <div>
-          <!-- header -->
-          接続されたデバイス
-          <div class="desc">Bluetooth、NFC</div>
-        </div>
-        <div>
-          <!-- content -->
-          <div class="wj-labeled-input switch">
-            <input id="dev1" type="checkbox" />
-            <label for="dev1"> AirPods Pro <span>イヤホン</span> </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="dev2" type="checkbox" />
-            <label for="dev2">
-              Apple Watch <span>スマートウォッチ</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="dev3" type="checkbox" />
-            <label for="dev3"> Magic Keyboard <span>キーボード</span> </label>
-          </div>
-          <button class="btn btn-primary">8個のデバイスをすべて表示...</button>
-        </div>
-      </wj-accordion-pane>
-
-      <wj-accordion-pane>
-        <!-- pane -->
-        <div>
-          <!-- header -->
-          アプリと通知
-          <div class="desc">最近使用したアプリ、デフォルトアプリ</div>
-        </div>
-        <div>
-          <!-- content -->
-          <div class="wj-labeled-input switch">
-            <input id="app-chrome" type="checkbox" />
-            <label for="app-chrome"> Chrome <span>Webブラウザ</span> </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="app-outlook" type="checkbox" />
-            <label for="app-outlook">
-              Outlook <span>メールと予定表</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="app-fb" type="checkbox" />
-            <label for="app-fb">
-              Facebook <span>ソーシャルメディア</span>
-            </label>
-          </div>
-          <button class="btn btn-primary">62個のアプリをすべて表示...</button>
-        </div>
-      </wj-accordion-pane>
-
-      <wj-accordion-pane>
-        <!-- pane -->
-        <div>
-          <!-- header -->
-          バッテリー
-          <div class="desc">67% - 残り5:30</div>
-        </div>
-        <div>
-          <!-- content -->
-          <div class="wj-labeled-input switch">
-            <input id="bat-sav" type="checkbox" />
-            <label for="bat-sav">
-              バッテリーセーバー
-              <span>バッテリー使用量を減らすために動作を遅くします。</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="bat-adapt" type="checkbox" />
-            <label for="bat-adapt">
-              適応バッテリー
-              <span>アプリがバッテリーを消耗したときに検出します。</span>
-            </label>
-          </div>
-          <div class="wj-labeled-input switch">
-            <input id="bat-pct" type="checkbox" />
-            <label for="bat-pct">
-              バッテリー残量 <span>バッテリーの割合を表示します。</span>
-            </label>
-          </div>
-        </div>
-      </wj-accordion-pane>
-    </wj-accordion>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import "@grapecity/wijmo.styles/wijmo.css";
-import "@grapecity/wijmo.vue2.nav";
-</script>
-
-<style>
-body {
-  margin-bottom: 3em;
-}
-
-/* customize the labeled inputs */
-.wj-accordion .wj-labeled-input {
-  width: 40%;
-}
-.wj-accordion .wj-labeled-input > label > span {
-  display: block;
-  font-weight: normal;
-  font-size: 90%;
-  opacity: 0.9;
-  color: black;
-}
-
-/* customize the accordion */
-.wj-accordion .wj-content {
-  border: 12px solid transparent;
-}
-
-/* color animation */
-.wj-accordion .wj-header {
-  transition: color 0.4s, background 0.4s;
-}
-
-/* header description */
-.wj-accordion .desc {
-  font-size: 9pt;
-  font-weight: normal;
-  color: #0085c7;
-}
-
-/* hide active header description */
-.wj-accordion .wj-header.wj-state-active .desc {
-  display: none;
-}
-
-/* give main pane a different color */
-.wj-accordion .main-pane.wj-state-active {
-  background: purple !important;
-}
-</style>
