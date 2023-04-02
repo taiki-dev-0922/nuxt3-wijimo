4fa75247b0e1ff1ae401add75dc3bd23602575e8
taiki-dev-0922 <taiki.dev0922@gmail.com>
2023-04-02 17:09:04 +0900

    プロジェクト作成、wijmoのグローバルインストールをpluginに追加

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/master/villages/list">村マスタ</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
---
+++ pages/master/villages/list.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>村</div>
+</template>
